b7ee1d41b60e75e7aea2472f008ca32c19eed1a9
juanchaco677 <31424234+juanchaco677@users.noreply.github.com>
2018-08-10 16:58:46 -0500

    actualizacion de servicio usuario

--- src/service/UsuarioService.java
+++ src/service/UsuarioService.java
@@ -10,7 +10,7 @@ import model.Usuario;
 
 public class UsuarioService {
 
-	public void crearUsuario(Usuario usuario){
+	public Registro crearUsuario(Usuario usuario) throws URISyntaxException{
 		Map<String,Object>parametros=new HashMap<>();				
 		parametros.put("departamento", usuario.getMesa().getPuntoVotacion().getLocalizacion().getCiudad().getDepartamento().getNombre());
 		parametros.put("ciudad", usuario.getMesa().getPuntoVotacion().getLocalizacion().getCiudad().getNombre());
@@ -22,43 +22,14 @@ public class UsuarioService {
 		parametros.put("passsword", usuario.getContrasena());
 		parametros.put("mesa", usuario.getMesa().getId());	
 
-		Registro registro;
-		try {
-			registro = Request.get("http://softwarevalerian.tk/api/usuario/crear", parametros);
-			if(registro!=null) {
-				boolean sucess=(boolean) registro.getCampos().get("sucess");
-				if(sucess) {
-					System.out.println("true");
-				}else {
-
-					System.out.println("false");
-				}
-			}
-		} catch (URISyntaxException e) {
-			e.printStackTrace();
-		}
-
+		return Request.get("http://softwarevalerian.tk/api/usuario/crear", parametros);
 	}
 
-	public void sesion(Usuario usuario){
+	public Registro sesion(Usuario usuario) throws URISyntaxException{
 		Map<String,Object>parametros=new HashMap<>();				
 		parametros.put("email", usuario.getCorreo());
-		parametros.put("passsword", usuario.getContrasena());
+		parametros.put("passsword", usuario.getContrasena());	
 
-		Registro registro;
-		try {
-			registro = Request.get("http://softwarevalerian.tk/api/usuario/sesion", parametros);
-			boolean sucess=(boolean) registro.getCampos().get("sucess");
-			if(sucess) {
-				System.out.println("true");
-			}else {
-
-				System.out.println("false");
-			}
-		} catch (URISyntaxException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
+		return Request.get("http://softwarevalerian.tk/api/usuario/sesion", parametros);
 	}
 }
